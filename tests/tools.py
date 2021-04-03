7df96a6d3e8c84715c4acb18ea74086863aff24e
Grimmys <funkyzermmy@gmail.com>
2021-04-03 11:06:50 +0200

    Added tests for cancelling movement after items traded

--- tests/tools.py
+++ tests/tools.py
@@ -4,7 +4,7 @@ import src.gui.fonts as font
 from src.constants import MAIN_WIN_WIDTH, MAIN_WIN_HEIGHT
 from src.game_entities.character import Character
 
-NB_TESTS_FOR_PROPORTIONS = 100
+NB_TESTS_FOR_PROPORTIONS = 1000
 
 
 def minimal_setup_for_game():
