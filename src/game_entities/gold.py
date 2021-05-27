512bddcfd7ea614de9c22cff906a3dd72b79d2ee
Grimmys <funkyzermmy@gmail.com>
2021-05-27 21:04:05 +0200

    Fixed gold initialization

--- src/game_entities/gold.py
+++ src/game_entities/gold.py
@@ -7,7 +7,7 @@ class Gold(Item):
     """
 
     def __init__(self, amount: int) -> None:
-        super().__init__(self, str(amount) + " Gold",
+        super().__init__(str(amount) + " Gold",
                          "imgs/dungeon_crawl/item/gold/gold_pile_10.png",
                          "Gold could be used to buy some items or other services", 0)
         self.amount: int = amount
