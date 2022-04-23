33c1656499588cb137550be317e96c60d5f95dd1
Grimmys <funkyzermmy@gmail.com>
2022-04-23 14:47:30 +0200

    Fix wrong paths for tilesets

---
+++ imgs/tiled_tilesets/thugs.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Thugs" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="32" height="32" source="../dungeon_crawl/monster/thugs/mutanter_cultist.png"/>
+ </tile>
+</tileset>
