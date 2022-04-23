c6d8136770773be705b7c4536f29fa3e5e1743c4
Grimmys <funkyzermmy@gmail.com>
2022-04-23 14:47:30 +0200

    Fix wrong paths for tilesets

---
+++ imgs/tiled_tilesets/dungeon.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="spritesheet" tilewidth="32" tileheight="32" tilecount="6080" columns="64">
+ <grid orientation="orthogonal" width="48" height="48"/>
+ <image source="dungeon.png" width="2048" height="3040"/>
+</tileset>
---
+++ imgs/tiled_tilesets/houses.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="houses" tilewidth="32" tileheight="32" tilecount="108" columns="6">
+ <grid orientation="orthogonal" width="48" height="48"/>
+ <image source="houses.png" width="192" height="576"/>
+</tileset>
