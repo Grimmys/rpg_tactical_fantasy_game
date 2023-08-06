3268cf68ffd5fd5d2ce6cb10525b844db2d2481e
Jim Zhou <csd_189@163.com>
2023-08-06 15:15:03 +0800

    delete leave_menu function

--- src/scenes/level_scene.py
+++ src/scenes/level_scene.py
@@ -1993,10 +1993,6 @@ class LevelScene(Scene):
             60,
         )
 
-    def leave_menu(self):
-        if self.menu_manager.active_menu is not None:
-            self.menu_manager.close_active_menu()
-
     def left_click(self, position: Position) -> None:
         """
         Handle the triggering of a left-click event.
