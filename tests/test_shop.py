86c4824144a33a95107f300c8b7ab006a1bf3fe3
CokeTree3 <zarinsmarcis104@gmail.com>
2024-06-18 12:49:26 +0200

    Unittest for Shop enhanced with buy feature testing

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -4,6 +4,7 @@ import unittest
 from src.game_entities.shop import Shop
 from tests.random_data_library import random_character_entity, random_item
 from tests.tools import minimal_setup_for_game
+from src.services.language import *
 
 
 class TestShop(unittest.TestCase):
@@ -44,9 +45,28 @@ class TestShop(unittest.TestCase):
         shop.interact(actor)
         # No assert for the moment
 
-    @unittest.skip
     def test_buy_item(self):
-        pass
+        name = "tavern"
+        pos = (3, 2)
+        sprite = "imgs/houses/blue_house.png"
+        shop_balance = 500
+        interaction = None
+        items = [
+            {"item": random_item(), "quantity": rd.randint(1, 10)},
+            {"item": random_item(), "quantity": rd.randint(1, 10)},
+            {"item": random_item(), "quantity": rd.randint(1, 10)},
+        ]
+        shop = Shop(name, pos, sprite, shop_balance, items, interaction)
+        actor = random_character_entity()
+        actor.gold = 10000
+        shop.interact(actor)
+        self.assertEqual(STR_THE_ITEM_HAS_BEEN_BOUGHT, shop.buy(items[0]["item"]))
+        actor.nb_items_max = len(actor.items)
+        self.assertEqual(STR_NOT_ENOUGH_SPACE_IN_INVENTORY_TO_BUY_THIS_ITEM, shop.buy(items[0]["item"]))
+        actor.nb_items_max = len(actor.items) + 1
+        actor.gold = 0
+        self.assertEqual(STR_NOT_ENOUGH_GOLD_TO_BY_THIS_ITEM, shop.buy(items[0]["item"]))
+
 
     @unittest.skip
     def test_buy_all_items(self):
