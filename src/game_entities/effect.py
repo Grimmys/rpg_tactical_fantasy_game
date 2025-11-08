85e5d79909c970be04205af4763c8741a3a35a6e
Léon Muselli <leon.muselli@etu.emse.fr>
2025-11-08 16:11:39 +0100

    refactor: alterations data to json

--- src/game_entities/effect.py
+++ src/game_entities/effect.py
@@ -4,10 +4,15 @@ Defines Effect class, corresponding to the application of a specific effect to a
 
 from __future__ import annotations
 
+import json
+
 from src.game_entities.alteration import Alteration
 from src.game_entities.destroyable import Destroyable
 from src.services.language import *
 
+# Load alteration data
+with open("data/alterations.json", "r", encoding="utf-8") as alterations_file:
+    ALTERATIONS_DATA = json.load(alterations_file)
 
 class Effect:
     """
@@ -30,26 +35,23 @@ class Effect:
         self.power: int = power
         self.duration: int = duration
         if self.name in ("speed_up", "strength_up", "defense_up"):
-            alteration_root = etree.parse("data/alterations.xml").find(name)
+            alteration_root = ALTERATIONS_DATA[self.name]
             desc = (
-                get_localized_string(alteration_root.find("info"))
+                get_localized_string(alteration_root["info"])
                 .strip()
                 .replace("{val}", str(self.power))
             )
-            abbr = alteration_root.find("abbreviated_name").text.strip()
+            abbr = alteration_root["abbreviated_name"].strip()
             self.alteration = Alteration(
                 self.name, abbr, self.power, self.duration, desc
             )
         elif self.name == "stun":
-            alteration_root = etree.parse("data/alterations.xml").find(name)
-            desc = get_localized_string(alteration_root.find("info")).strip()
-            abbr = alteration_root.find("abbreviated_name").text.strip()
-            effs_el = alteration_root.find("effects")
-            durable_effects = (
-                effs_el.text.strip().split(",") if effs_el is not None else []
-            )
+            alteration_root = ALTERATIONS_DATA[self.name]
+            desc = get_localized_string(alteration_root["info"]).strip()
+            abbr = alteration_root["abbreviated_name"].strip()
+            effects = alteration_root.get("effects", [])
             self.alteration = Alteration(
-                self.name, abbr, self.power, self.duration, desc, durable_effects
+                self.name, abbr, self.power, self.duration, desc, effects
             )
 
     def apply_on_ent(self, entity: Destroyable) -> tuple[bool, str]:
